22fe4bb3d42e083af71b6acb453762c8d2e90746
Sree Narayanan <sreeaadhi07@gmail.com>
2025-10-07 13:02:51 +0400

    Update http.ts

--- packages/backend/convex/http.ts
+++ packages/backend/convex/http.ts
@@ -1,6 +1,7 @@
 import { httpRouter } from "convex/server";
 import { authComponent, createAuth } from "./auth";
 import { handleGitHubWebhook } from "./github/webhooks";
+import { handlePolarWebhook } from "./polar/http";
 
 const http = httpRouter();
 
@@ -10,6 +11,12 @@ http.route({
   path: "/github/webhook",
   method: "POST",
   handler: handleGitHubWebhook,
+});
+
+http.route({
+  path: "/polar/webhook",
+  method: "POST",
+  handler: handlePolarWebhook,
 });
 
 export default http;
