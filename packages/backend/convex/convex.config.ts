397ad85cb4298f79178812cfa0086a73a968a2dc
Sree Narayanan <sreeaadhi07@gmail.com>
2025-10-24 16:50:31 +0400

    Update convex.config.ts

--- packages/backend/convex/convex.config.ts
+++ packages/backend/convex/convex.config.ts
@@ -1,7 +1,9 @@
 import betterAuth from "@convex-dev/better-auth/convex.config";
+import dodopayments from "@dodopayments/convex/convex.config";
 import { defineApp } from "convex/server";
 
 const app = defineApp();
 app.use(betterAuth);
+app.use(dodopayments);
 
 export default app;
